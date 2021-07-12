e1f6ec683d817d3b966cd232d1c4fb9177a426a4
SomeGuy3000 <lustyk12@wp.pl>
2021-07-12 05:04:04 +0200

    From now table is fully generated

--- src/components/MiddlePopup.vue
+++ src/components/MiddlePopup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="MiddlePopup m-6">
     <div class="max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-3xl p-4">
-        <Table endpoint="https://jsonplaceholder.typicode.com/users" seaech="true" sorting="true" pagination="false"/>
+        <Table endpoint="https://jsonplaceholder.typicode.com/users" displayOrder="name,email,company.name,address.city,website" seaech="true" sorting="true" pagination="false"/>
     </div>
   </div>
 </template>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,20 +3,21 @@
     <table class="table-auto border-collapse w-full">
       <thead>
         <tr class="text-gray-600">
-          <th>Name</th>
-          <th>Email</th>
-          <th>Company name</th>
-          <th>City</th>
-          <th>Website</th>
+          <th v-for="path in splitDisplayOrder" :key="path">{{capitalizeFirstLetter(path.replace(/\./g,' '))}}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="obj in endpointRes" :key="obj" class="even:bg-gray-100">
-            <td class="border border-gray-600 px-2 py-1">{{obj.name}}</td>
-            <td class="border border-gray-600 px-2 py-1">{{obj.email}}</td>
-            <td class="border border-gray-600 px-2 py-1">{{obj.company.name}}</td>
-            <td class="border border-gray-600 px-2 py-1">{{obj.address.city}}</td>
-            <td class="border border-gray-600 px-2 py-1">{{obj.website}}</td>
+          <td class="border border-gray-600 px-2 py-1" v-for="path in splitDisplayOrder" :key="path" >
+            <div v-if="path == 'email'">
+              <a class="underline" v-bind:href="'mailto:' + obj[path]">
+                {{getJsonElByPath(obj, path.split('.'))}}
+              </a>
+            </div>
+            <div v-else>
+              {{getJsonElByPath(obj, path.split('.'))}}
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -30,20 +31,35 @@ export default {
   name: 'Label',
   props: {
     endpoint: String,
+    displayOrder: String,
     search: Boolean,
     sorting: Boolean,
     pagination: Boolean,
   },
   data () {
     return {
-      endpointRes: null
+      endpointRes: null,
+      splitDisplayOrder: this.displayOrder.split(",")
     }
-  },  
+  },
+  methods: {
+    capitalizeFirstLetter (string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    getJsonElByPath (obj, path) {  
+      if (path && obj) {
+        path.forEach(function(key){
+          obj = obj[key];
+        });
+        console.log(obj)
+        return obj;
+      }
+    }
+  },
   mounted () {
     axios
       .get(this.endpoint)
       .then(response => {this.endpointRes = response.data
-      console.log(this.endpointRes)
       })
   }
 }
